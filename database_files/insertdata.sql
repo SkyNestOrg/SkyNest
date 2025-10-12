1c44647818fd1c76c433865330be346678ff2aaa
Dinurang <dinura1ginige@gmail.com>
2025-10-12 19:57:25 +0530

    Update insertdata.sql

--- database_files/insertdata.sql
+++ database_files/insertdata.sql
@@ -1,45 +1,7 @@
 -- insert data for tables
 
--- Insert Room data
-INSERT INTO `Room` (`room_number`, `current_status`, `room_type`, `branch_id`) VALUES
--- Branch 1 Rooms
-(101, 'Available', 'Single', 1),
-(102, 'Available', 'Single', 1),
-(103, 'Available', 'Double', 1),
-(104, 'Available', 'Double', 1),
-(105, 'Available', 'Deluxe', 1),
-(106, 'Available', 'Deluxe', 1),
-(107, 'Available', 'Suite', 1),
-(108, 'Available', 'Suite', 1),
-(109, 'Available', 'Budget', 1),
-(110, 'Available', 'Budget', 1),
 
--- Branch 2 Rooms
-(201, 'Available', 'Single', 2),
-(202, 'Available', 'Single', 2),
-(203, 'Available', 'Double', 2),
-(204, 'Available', 'Double', 2),
-(205, 'Available', 'Deluxe', 2),
-(206, 'Available', 'Deluxe', 2),
-(207, 'Available', 'Suite', 2),
-(208, 'Available', 'Suite', 2),
-(209, 'Available', 'Budget', 2),
-(210, 'Available', 'Budget', 2),
-
--- Branch 3 Rooms
-(301, 'Available', 'Single', 3),
-(302, 'Available', 'Single', 3),
-(303, 'Available', 'Double', 3),
-(304, 'Available', 'Double', 3),
-(305, 'Available', 'Deluxe', 3),
-(306, 'Available', 'Deluxe', 3),
-(307, 'Available', 'Suite', 3),
-(308, 'Available', 'Suite', 3),
-(309, 'Available', 'Budget', 3),
-(310, 'Available', 'Budget', 3);
-
-
---insert the required RoomType data
+-- insert the required RoomType data
 INSERT INTO roomtype (type_name, base_price, amenities, capacity) VALUES
 ('Budget', 13500.00, 'Family Bed, WiFi, Air Conditioning, TV (50°), Mini-Fridge, Desk, Wardrobe, In-room Safe, Free Toiletries', 5),
 ('Deluxe', 12000.00, 'King Bed, High-Speed WiFi, Air Conditioning, Smart TV (55°) with Streaming, Private Bathroom with Bathtub and Shower, Premium Mini-Bar, Espresso Machine, Sitting Area, Wardrobe, In-room Safe, Robe, Slippers, Premium Toiletries, Hair Dryer, Iron, Ironing Board', 4),
@@ -48,14 +10,17 @@ INSERT INTO roomtype (type_name, base_price, amenities, capacity) VALUES
 ('Suite', 20000.00, 'King Bed, Ultra High-Speed WiFi, Climate Control AC, Smart TV (65°) + Second TV, Private Bathroom with Jacuzzi and Rain Shower, Separate Living Room, Kitchenette (sink, microwave, Nespresso machine), Dining Table, Stocked Premium Mini-Bar, Work Office Area', 4);
 
 
---branches
+
+-- branches
 INSERT INTO `Branch` (`branch_id`, `branch_name`, `address`, `city`, `contact_number`) VALUES
 (1, 'SkyNest Urban', '123 Galle Face', 'Colombo 01', '0112345678'),
 (2, 'SkyNest Coast', '456 Galle Road', 'Galle', '0912345679'),
 (3, 'SkyNest Hills', '789 Colombo Road', 'Kandy', '0812345680');
 
 
---services
+
+
+-- services
 INSERT INTO service(service_type, unit_quantity_charges, branch_id, availability) VALUES
 -- Branch 1
 ('Airport Drop', 9700.00, 1, 'No'),
@@ -98,17 +63,62 @@ INSERT INTO service(service_type, unit_quantity_charges, branch_id, availability
 ('wifi - upgrade', 3700.00, 3, 'No');
 
 
---initial staff-users
+-- initial staff-users
 
-INSERT INTO staff_user(username, password, official_role, branch_id) VALUES
-('AD001', '$2a$12$Sr2/RCxElkZyPWbwYal58.R.1qwyPB8dFCIbfh13RWUJVQwEG0U6i', 'admin-user', 1),
-('FO001', '$2b$12$e4fCILYKTTDWW48UJ9AJyVGif1Ee0H7RyS7zssxGtY1B8i6LdW3C', 'frontoffice-user', 1),
-('FO002', '$2b$12$rtUjGEYJJs/0M/WcdrQETeyG32wfhyUB6WAsmek1/RS5as5fYSq2ve', 'frontoffice-user', 2),
-('MN001', '$2a$12$Sr2/RCxElkZyPWbwYal58.R.1qwyPB8dFCIbfh13RWUJVQwEG0U6i', 'management-user', 1),
-('SO001', '$2a$12$Sr2/RCxElkZyPWbwYal58.R.1qwyPB8dFCIbfh13RWUJVQwEG0U6i', 'serviceoffice-user', 1),
-('SO002', '$2a$12$Sr2/RCxElkZyPWbwYal58.R.1qwyPB8dFCIbfh13RWUJVQwEG0U6i', 'serviceoffice-user', 2);
 
---note that all these hashed passwords =  123456.
+INSERT INTO staff_user (username, password, official_role, branch_id) VALUES
+('AD001', '$2a$12$wO7zUDW2gFRu34poUSa.q.5T2eqgAOVQxkFik59aSz.kqV9vIL2h.', 'admin-user', 1),
+('FO001', '$2a$12$wO7zUDW2gFRu34poUSa.q.5T2eqgAOVQxkFik59aSz.kqV9vIL2h.', 'frontoffice-user', 1),
+('FO002', '$2a$12$wO7zUDW2gFRu34poUSa.q.5T2eqgAOVQxkFik59aSz.kqV9vIL2h.', 'frontoffice-user', 2),
+('FO003', '$2a$12$wO7zUDW2gFRu34poUSa.q.5T2eqgAOVQxkFik59aSz.kqV9vIL2h.', 'frontoffice-user', 3),
+('MN001', '$2a$12$wO7zUDW2gFRu34poUSa.q.5T2eqgAOVQxkFik59aSz.kqV9vIL2h.', 'management-user', 1),
+('SO001', '$2a$12$wO7zUDW2gFRu34poUSa.q.5T2eqgAOVQxkFik59aSz.kqV9vIL2h.', 'serviceoffice-user', 1),
+('SO002', '$2a$12$wO7zUDW2gFRu34poUSa.q.5T2eqgAOVQxkFik59aSz.kqV9vIL2h.', 'serviceoffice-user', 2),
+('SO003', '$2a$12$wO7zUDW2gFRu34poUSa.q.5T2eqgAOVQxkFik59aSz.kqV9vIL2h.', 'serviceoffice-user', 3);
+
+-- note that all these hashed passwords =  123456.
+
+
+-- Insert Room data
+INSERT INTO `Room` (`room_number`, `current_status`, `room_type`, `branch_id`) VALUES
+-- Branch 1 Rooms
+(101, 'Available', 'Single', 1),
+(102, 'Available', 'Single', 1),
+(103, 'Available', 'Double', 1),
+(104, 'Available', 'Double', 1),
+(105, 'Available', 'Deluxe', 1),
+(106, 'Available', 'Deluxe', 1),
+(107, 'Available', 'Suite', 1),
+(108, 'Available', 'Suite', 1),
+(109, 'Available', 'Budget', 1),
+(110, 'Available', 'Budget', 1),
+
+-- Branch 2 Rooms
+(201, 'Available', 'Single', 2),
+(202, 'Available', 'Single', 2),
+(203, 'Available', 'Double', 2),
+(204, 'Available', 'Double', 2),
+(205, 'Available', 'Deluxe', 2),
+(206, 'Available', 'Deluxe', 2),
+(207, 'Available', 'Suite', 2),
+(208, 'Available', 'Suite', 2),
+(209, 'Available', 'Budget', 2),
+(210, 'Available', 'Budget', 2),
+
+-- Branch 3 Rooms
+(301, 'Available', 'Single', 3),
+(302, 'Available', 'Single', 3),
+(303, 'Available', 'Double', 3),
+(304, 'Available', 'Double', 3),
+(305, 'Available', 'Deluxe', 3),
+(306, 'Available', 'Deluxe', 3),
+(307, 'Available', 'Suite', 3),
+(308, 'Available', 'Suite', 3),
+(309, 'Available', 'Budget', 3),
+(310, 'Available', 'Budget', 3);
+
+
+
 
 
 
