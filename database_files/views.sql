27666e2bc53216a5a152df5ef076976f2199e59d
Dinurang <dinura1ginige@gmail.com>
2025-10-12 19:59:24 +0530

    Update views.sql

--- database_files/views.sql
+++ database_files/views.sql
@@ -1,4 +1,4 @@
---this view used in ViewDueServices.js in Service Office End
+-- this view used in ViewDueServices.js in Service Office End
 
 CREATE VIEW DueServicesView AS
 SELECT 
@@ -15,21 +15,35 @@ LEFT JOIN service s ON sr.request_type = s.service_type AND sr.branch_id = s.bra
 WHERE sr.status = 'Request Placed'
 ORDER BY sr.date_time ASC;
 
----------------------------------------
+-- -------------------------------------
 
 CREATE VIEW view_all_rooms AS
 SELECT 
     r.room_number
 FROM room r;
 
----------------------------------------
+-- -------------------------------------
 
 CREATE VIEW view_all_services AS
 SELECT distinct s.service_type
 FROM service s;
 
----------------------------------------
+-- -------------------------------------
 
 CREATE VIEW view_all_branches AS
 SELECT distinct b.branch_name 
 FROM branch b;
+
+-- -------------------------------------
+
+CREATE VIEW latest_tax_percentage AS
+SELECT 
+    latest_tax_percentage
+FROM taxes_and_charges
+WHERE revision_id = (
+    SELECT revision_id 
+    FROM taxes_and_charges 
+    WHERE revision_date IS NOT NULL 
+    ORDER BY revision_date DESC 
+    LIMIT 1
+);
